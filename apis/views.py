7bd8d694039e293b74dad9d5421403ee17d8bdea
murathe <isaacsm07p@gmail.com>
2020-02-27 11:45:34 +0300

    Create GRoup endpoint

--- apis/views.py
+++ apis/views.py
@@ -3,9 +3,16 @@ from rest_framework import viewsets
 from apis.serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
-class GroupViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     '''
     API endpoint that allows users to be viewwe or edited.
     '''
     queryset = User.objects.all().order_by('date_joined')
     serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    '''
+    API endpoint that allows groups to be viewed or edited
+    '''
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
