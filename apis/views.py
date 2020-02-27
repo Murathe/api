76fad23ff4aa243d67be505f3a9817b8815ccf73
murathe <isaacsm07p@gmail.com>
2020-02-27 11:40:43 +0300

    CReate endpoint one fr group

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from apis.serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
+class GroupViewSet(viewsets.ModelViewSet):
+    '''
+    API endpoint that allows users to be viewwe or edited.
+    '''
+    queryset = User.objects.all().order_by('date_joined')
+    serializer_class = UserSerializer
