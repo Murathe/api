7219c0469627343fee20c093ccf5e980bad88521
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-28 10:45:05 +0300

    modified

--- api/urls.py
+++ api/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('apis.urls')),
     path('social-auth/', include('social_django.urls', namespace="social")),
+    path(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
